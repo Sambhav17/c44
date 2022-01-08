d0f6014650dfa0cce859b745d3d39fb984650bd9
Sambhav17 <88580102+Sambhav17@users.noreply.github.com>
2022-01-08 15:25:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var canvas;
+var backgroundImage, car1_img,car1_boost, car2_img,car2_boost, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("./stripe line.jpg");
+  car1_img = loadImage("./jecko2.png");
+  car1_boost=loadImage("./jeckoboost.png")
+  car2_img = loadImage("./devel162.png");
+  car2_boost=loadImage("./devel16boost.png")
+  track = loadImage("./stripe line.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+}    
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+    
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
